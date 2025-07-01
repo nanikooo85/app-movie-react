d58179d7c40b4449fccd4a8f2a1f9a87aca134b2
Nana Sakhokia <137408995+nanikooo85@users.noreply.github.com>
2025-07-02 01:48:47 +0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import MovieDetails from "./pages/MovieDetails";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movie/:id" element={<MovieDetails />} />
+    </Routes>
+  );
+};
+
+export default App;
